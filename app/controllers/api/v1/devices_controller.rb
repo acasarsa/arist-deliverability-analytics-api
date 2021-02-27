56180e0cad32989c3cea609cb7db30a082666e4a
github username <github email address>
2021-02-27 14:19:48 -0500

    refactor to tests

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -7,12 +7,8 @@ module Api
       end
 
       def register
-        if params['disabled_at'] == nil #maybe don't need that is there any way to hit the post request if you already have it?
           device = Device.create(device_params)
           render json: device
-        else 
-          render json: {"error": "This divice is disabled"}, status: 500
-        end
       end
 
       def terminate 
@@ -31,11 +27,27 @@ module Api
         end
       end
 
+      def report 
+        device = Device.find(params[:device_id])
+        if !device.disabled_at
+          # may consider setting sender: device.phone_number unless this is supposed to be a name or whatever the user sets it to be
+          # binding.pry
+          report = Report.create(sender: params[:sender], message: params[:message], device_id: params[:device_id])
+          render json: report
+        else 
+          render json: {"error": "This divice is disabled"}, status: 500
+        end
+      end
+
       private
 
       def device_params
         params.require(:device).permit(:phone_number, :carrier, :disabled_at)
       end
+
+      # def report_params
+      #   params.require(:report).permit(:sender, :message, :device_id)
+      # end
     end
   end
 end
--- spec/requests/devices_spec.rb
+++ spec/requests/devices_spec.rb
@@ -2,37 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Devices", type: :request do
   describe "POST /api/v1/register" do 
-    it "creates device with status 200" do 
-      post "/api/v1/register", :params => { 
-        "device":  { 
-          "phone_number": "1231231234", 
-          "carrier": "Cell-ONE"
-        }}
+    it "creates device with status 200 and responds to JSON by default" do 
+      post "/api/v1/register", params: { device: { phone_number: "1231231234", carrier: "Cell-ONE"}}
         expect(response).to have_http_status(200)
-    end
-
-    it "responds to JSON by default" do 
-      post "/api/v1/register", :params => { 
-        "device":  { 
-          "phone_number": "1231231234", 
-          "carrier": "Cell-ONE"
-        }}
         expect(response.content_type).to eq("application/json; charset=utf-8")
     end
-    # it "returns status 500 error if device is disabled" do 
-    #   post "/api/v1/register", :params => { 
-    #     "device":  { 
-    #       "phone_number": "1231231234", 
-    #       "carrier": "Cell-TWO",
-    #       "disabled_at": "2021-02-27T00:22:29.781Z"
-    #     }}
-    #     expect(response).to have_http_status(500)
-    # end
   end
 
   describe "PATCH /api/v1/terminate" do 
     it "updates disabled_at to current timestamp" do
-      device = Device.create(phone_number:  "1231231234", carrier: "XCL", disabled_at: nil)
+      device = Device.create(phone_number: "+1231231234", carrier: "XCL", disabled_at: nil)
       patch '/api/v1/terminate', params: { id: device.id, disabled_at: Time.now }
       expect(response).to have_http_status(200)
     end
@@ -40,18 +19,34 @@ RSpec.describe "Devices", type: :request do
 
   describe "POST /api/v1/alive" do 
     it "creates a heartbeat with status 200 when device is not disabled" do 
-      device = Device.create(phone_number:  "1231231234", carrier: "XCL", disabled_at: nil)
+      device = Device.create(phone_number: "+12231231234", carrier: "XCL", disabled_at: nil)
       post '/api/v1/alive', params: {device_id: device.id}
       expect(response).to have_http_status(200)
     end
 
     it "throws an error and returns status 500 if device is disabled" do 
-      device = Device.create(phone_number:  "1231231234", carrier: "XCL", disabled_at: nil)
-      patch '/api/v1/terminate', params: { id: device.id, disabled_at: Time.now }
+      device = Device.create(phone_number: "+12231231234", carrier: "XCL", disabled_at: Time.now)
       post '/api/v1/alive', params: {device_id: device.id}
       expect(response).to have_http_status(500)
     end
 
+    describe "POST /api/v1/report" do
+      it "creates a report with status 200 when device is not disabled and allows for utf-8 text" do 
+        device = Device.create(phone_number: "+12231231234", carrier: "XCL", disabled_at: nil)
+        post '/api/v1/alive', params: {device_id: device.id, sender: '+12231231234', message: 'send dog pics 🐕🐕🐕'}
+        expect(response).to have_http_status(200)
+      end
+
+      it "throws an error and returns status 500 if device is disabled" do 
+        device = Device.create(phone_number: "+12231231234", carrier: "XCL", disabled_at: Time.now)
+        post '/api/v1/alive', params: {device_id: device.id, sender: '+12231231234', message: 'send dog pics'}
+        expect(response).to have_http_status(500)
+      end
+    end
+
+    # make test for the correct phone number format 
+    # see about fixing the test for device creation
+    # make the first tests neater like these ones
   end
 
 
