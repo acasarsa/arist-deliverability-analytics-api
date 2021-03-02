99414ef185367948ea59f6b2807b7d42480d88b4
github username <github email address>
2021-03-01 22:31:15 -0500

    added a default_scope to order json by created_at -- fixes issue where devices json is reordered by updated_at after disabling a device

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,5 @@ class ApplicationRecord < ActiveRecord::Base
 
   # overides defualt ordering on models with UUID primary keys so that .first / .last work 
   self.implicit_order_column = :created_at
+  default_scope { order('created_at') } # json will be ordered by created_at 
 end
