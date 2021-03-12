60f2d9ec9ebcfb9032a12557c1e9c3b9282ef141
github username <github email address>
2021-03-12 13:22:53 -0500

    comments

--- app/models/device.rb
+++ app/models/device.rb
@@ -10,5 +10,6 @@ class Device < ApplicationRecord
   
   def normalize_phone 
     self.phone_number = Phonelib.parse(phone_number).e164
+    # went with .e164 instead of .full_e164 because the directions didn't specifcally mention needing an extension & since the app is for text message based communication, I made the judgement call that it wasn't needed. However, this is somehthing I'd ask my senior about in practice. 
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,8 @@ class Report < ApplicationRecord
   belongs_to :device
 
   validates :sender, phone: {possible: false, message: "phone number is invalid"}
+  # only validated for things that were asked for in the deliverables to save time. 
+  # If this was a ticket and didn't specify I would ask for clarifaction. 
 
   private 
 
